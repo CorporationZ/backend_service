6a88dca0384937bdfcb453727f00f9b484bde4aa
Muhammadsolih Abdugafforov <salikhdev@gmail.com>
2025-02-11 17:25:45 +0500

    start up

---
+++ src/main/java/uz/salikhdev/backend_service/BackendServiceApplication.java
@@ -0,0 +1,13 @@
+package uz.salikhdev.backend_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendServiceApplication.class, args);
+    }
+
+}
